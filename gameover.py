1781ce1a277c8a1442f4fc88a1f404916fedcd24
Hasan Majid <hasanmajid3.14@hotmail.com>
2023-08-06 15:16:14 -0400

    centered game over text

--- gameover.py
+++ gameover.py
@@ -6,22 +6,12 @@ class GameOver:
         self.status = False
         self.screen = screen
         self.width = width
-        self.font = pg.font.Font("freesansbold.ttf", 32)
+        self.font = pg.font.Font("freesansbold.ttf", 40)
 
     def show_game_over(self):
-        self.screen.blit(
-            self.font.render("Game Over", True, "black", "red"),
-            pg.draw.rect(
-                self.screen,
-                "orange",
-                pg.Rect(
-                    self.width / 2,
-                    self.width / 2,
-                    0,
-                    0,
-                ),
-            ),
-        )
-    
+        text = self.font.render("Game Over!", True, "black", "red")
+        text_rect = text.get_rect(center = (self.width/2, self.width / 2),)
+        self.screen.blit(text, text_rect)
+
     def reset(self):
         self.status = False
