61cd21276a491bd6af996bea9e40344f957b4e3c
Hasan Majid <hasanmajid3.14@hotmail.com>
2023-08-06 02:04:10 -0400

    created and used game over object

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ from snake import Snake
 from food import Food
 from grid import Grid
 from scoreboard import Scoreboard
+from gameover import GameOver
 
 
 class Game:
@@ -23,12 +24,12 @@ class Game:
         screen = pg.display.set_mode((width, height))
         clock = pg.time.Clock()
         running = True
-        game_over = False
 
         # Creating game objects
         scoreboard = Scoreboard(screen, width)
         snake = Snake(grid_size)
         food = Food(grid_size, snake.trail)
+        game_over = GameOver(screen, width)
 
         start_ticks = pg.time.get_ticks()  # starter tick
 
@@ -40,6 +41,14 @@ class Game:
                     running = False
 
             keys = pg.key.get_pressed()
+
+            # Restarts game if the r key is pressed
+            if keys[pg.K_r] and game_over.status == True:
+                game_over.reset()
+                snake.reset()
+                scoreboard.reset()
+                food.reset()
+
             if len(snake.trail) > 1:
                 if keys[pg.K_w] and snake.trail[0].y - 1 != snake.trail[1].y:
                     snake.dir = "up"
@@ -64,7 +73,7 @@ class Game:
             ) / 1000  # calculate how many seconds
 
             # Moving snake every 0.2 seconds
-            if seconds >= 0.15 and game_over == False:
+            if seconds >= 0.15 and game_over.status == False:
                 start_ticks = pg.time.get_ticks()  # starter tick
                 if snake.dir == "up":
                     snake.move_up()
@@ -78,16 +87,16 @@ class Game:
                 # Checking for collision
                 if snake.trail[0].x == 0 or snake.trail[0].x == grid_size - 1:
                     print("Game Over")
-                    game_over = True
+                    game_over.status = True
                 elif snake.trail[0].y == 0 or snake.trail[0].y == grid_size - 1:
                     print("Game Over")
-                    game_over = True
-                
+                    game_over.status = True
+
                 # Checking if snake collides with itself
                 for i in range(2, len(snake.trail)):
                     if snake.trail[0] == snake.trail[i]:
                         print("Game Over")
-                        game_over = True
+                        game_over.status = True
 
             # fill the screen with a color to wipe away anything from last frame
             screen.fill("black")
@@ -105,7 +114,7 @@ class Game:
             if snake.trail[0] == food:
                 snake.grow()
                 food = Food(grid_size, snake.trail)
-                scoreboard.score += 1 # Incrementing score
+                scoreboard.score += 1  # Incrementing score
 
             # Checking if there is a new highscore and updating when there is
             if scoreboard.score > scoreboard.highscore:
@@ -113,6 +122,10 @@ class Game:
 
             # Drawing scoreboard
             scoreboard.draw_score()
+
+            # Displaying game over if lost
+            if (game_over.status == True):
+                game_over.show_game_over()
 
             # flip() the display to put your work on screen
             pg.display.flip()
